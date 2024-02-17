e24f83c3b0a4f3ffa83586c09700618ca595e93f
Esa Kataja <esa.kataja@proton.me>
2024-02-17 13:15:58 +0200

    ADD Indexer progressbar

--- src/video_indexer.py
+++ src/video_indexer.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from argparse import ArgumentParser
 from rich import print
+from rich.progress import track
 from csv import DictWriter
 from typing import List
 import json
@@ -27,35 +28,48 @@ def get_videofile_list(input_folder: Path):
 
 def index_video_files(video_list: List[Path]) -> dict:
     video_list_data = list()
-    for i,videofile in enumerate(video_list):
+    number_of_files = len(video_list)
+    for videofile in track(video_list, description=f"Found {number_of_files} files. Indexing..."):
+
         try:
             video_data = get_video_data(videofile)
             video_data_add = {
+                "Title": video_data.general_video_info.tags.title,
+                "date_released": video_data.general_video_info.tags.DATE_RELEASED,
+                "imdb_id": video_data.general_video_info.tags.IMDB_ID,
+                "original_media_type": "",
                 "Filename": videofile.name,
                 "Filesize": videofile.stat().st_size,
                 "MPixels": video_data.video_streams[0].MPixels,
                 "Codec": video_data.video_streams[0].codec_name,
+                "Format": video_data.general_video_info.format_name,
                 "Resolution": f"{video_data.video_streams[0].width}x{video_data.video_streams[0].height}",
                 "FPS": round(video_data.video_streams[0].fps, 2)
             }
+
             video_list_data.append(video_data_add)
-            print(f"{i+1}/{len(video_list)}")
         except Exception as e:
             print("Video giving error: ", videofile, "Codec: ", video_data.video_streams[0].codec_name,"\n")
             print(e)
     return video_list_data
 
+def write_csv(video_list_data):
+    with open("video_index.csv", "w") as f:
+        writer = DictWriter(f, fieldnames=video_list_data[0].keys())
+        writer.writeheader()
+        writer.writerows(video_list_data)
+
+def write_json(video_list_data):
+    with open("video_index.json", "w") as f:
+        json.dump(video_list_data, f, indent=4)
+
 def main()->None:
     args = parse_args()
     videofile_list: List[Path] = get_videofile_list(args.input_folder)
-    files_found = len(videofile_list)
-    print(f"Found {files_found} files.","Starting indexing...")
     indexed_list = index_video_files(videofile_list)
     print("Indexing complete!")
-    with open("video_index.csv", "w") as f:
-        writer = DictWriter(f, fieldnames=indexed_list[0].keys())
-        writer.writeheader()
-        writer.writerows(indexed_list)
+    write_csv(indexed_list)
+    write_json(indexed_list)
         
 
     
